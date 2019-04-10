b54a5889f4d762003fc851b181b5455b9035f5a6
Alexandru <alexandru.simion013@gmail.com>
2019-04-10 22:07:34 +0300

    a reveniit

---
+++ ISSgui/ISSgui/Domain/ISwitchToAuthor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+<<<<<<< HEAD
+namespace ISSgui.Domain {
+    interface ISwitchToAuthor {
+    }
+}
+=======
+namespace ISSgui.Domain
+{
+    interface ISwitchToAuthor
+    {
+        void SwitchToAuthor();
+    }
+}
+>>>>>>> feature/Domain/Listener
