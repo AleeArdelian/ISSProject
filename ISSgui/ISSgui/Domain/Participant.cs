d243c30acb6161f204296368f5e2944e654b4c49
Catalin Belcianu <catalin.belcianu@gmail.com>
2019-04-10 21:24:36 +0300

    implemented Author and Speaker

--- ISSgui/ISSgui/Domain/Participant.cs
+++ ISSgui/ISSgui/Domain/Participant.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace ISSgui.Domain
 {
-    class Participant
+    public abstract class Participant
     {
+        private string username;
+        private string password;
+        private string firstName;
+        private string lastName;
+        private string email;
+
+        public string getUsername() { return username; }
+        public string getPassword() { return password; }
+        public string getFirstName() { return firstName; }
+        public string getLastName() { return lastName; }
+        private string getEmail() { return email; }
     }
 }
