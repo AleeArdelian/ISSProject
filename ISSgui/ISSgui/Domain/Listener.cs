0e596ad368cf262d4f602ba504d52f4d2f0866b2
Alexandru <alexandru.simion013@gmail.com>
2019-04-10 21:32:08 +0300

    Implemented

---
+++ ISSgui/ISSgui/Domain/Listener.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISSgui.Domain
+{
+    class Listener
+    {
+        private long cNP;
+
+        public long CNP { get => cNP; set => cNP = value; }
+    }
+}
