b8617d650999b8f805ee5fe6db043f8169a88124
Catalin Belcianu <catalin.belcianu@gmail.com>
2019-05-14 20:18:14 +0300

    implemented register

---
+++ Application/ConferenceManagementSystem/Server/Repository/ChosenPCRepo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Domain;
+using Dapper;
+
+namespace Server.Repository
+{
+    class ChosenPCRepo : IRepository<ChosenPCMember>
+    {
+        public void Add(ChosenPCMember entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ChosenPCMember> FindAll()
+        {
+            using (IDbConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["cmsDatabase"].ConnectionString))
+            {
+                return db.Query<ChosenPCMember>("SELECT * FROM ChosenProgramCommittee").ToList();
+            }
+        }
+
+        public ChosenPCMember FindOne(ChosenPCMember entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(ChosenPCMember entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ChosenPCMember entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Application/ConferenceManagementSystem/Server/Service/CMSService.cs
+++ Application/ConferenceManagementSystem/Server/Service/CMSService.cs
@@ -13,6 +13,7 @@ namespace Server.Service
     {
         ConferenceRepository confRepo = new ConferenceRepository();
         RegularMemberRepository rmRepo = new RegularMemberRepository();
+        ChosenPCRepo cpcRepo = new ChosenPCRepo();
 
         public void AddConference(int ConferenceId, string ConferenceName)
         {
@@ -86,6 +87,35 @@ namespace Server.Service
         public void UpdateSpeaker(string CNP, string affiliation, string username, string password, string firstName, string lastName, string email)
         {
             throw new NotImplementedException();
+        }
+
+        public bool register(string firstName, string lastName, string CNP, string affiliation, string website, string email, string username, string password)
+        {
+            List<ChosenPCMember> allmemb = cpcRepo.FindAll();
+            String role="";
+            bool ok = false;
+            foreach (ChosenPCMember x in allmemb)
+            {
+                if (x.getCNP() == CNP)
+                {
+                    ok = true;
+                    role = x.getPCRole();
+                }
+            }
+
+            if (ok)
+            {
+                if (role.Equals("RegularMember")) {
+                    RegularMember rm = new RegularMember(CNP, affiliation, website);
+                    rm.setEmail(email); rm.setFirstName(firstName); rm.setLastName(lastName); rm.setUsername(username); rm.setPassword(password);
+                    rmRepo.Add(rm);
+                }
+                return ok;
+            }
+            else
+            {
+                return !ok;
+            }
         }
     }
 }
